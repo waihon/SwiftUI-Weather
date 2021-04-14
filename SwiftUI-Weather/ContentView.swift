6d1b890e6731252eaa13bbce7ca0a0d1e62a3695
Waihon Yew <yewwaihon@gmail.com>
2021-04-14 22:34:20 +0800

    Create an initial version of app background

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    ZStack {
+      LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                     startPoint: .topLeading,
+                     endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
+    }
   }
 }
 
