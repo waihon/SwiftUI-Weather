9e027d9546d361bae7d9478814a2569fa1b472af
Waihon Yew <yewwaihon@gmail.com>
2021-04-15 20:45:26 +0800

    Complete the remaining 4 weather day views

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -35,6 +35,22 @@ struct ContentView: View {
           WeatherDayView(dayOfWeek: "TUE",
                          imageName: "cloud.sun.fill",
                          temperature: 74)
+          
+          WeatherDayView(dayOfWeek: "WED",
+                         imageName: "sun.max.fill",
+                         temperature: 88)
+          
+          WeatherDayView(dayOfWeek: "THU",
+                         imageName: "wind.snow",
+                         temperature: 55)
+          
+          WeatherDayView(dayOfWeek: "FRI",
+                         imageName: "sunset.fill",
+                         temperature: 60)
+          
+          WeatherDayView(dayOfWeek: "SAT",
+                         imageName: "snow",
+                         temperature: 25)
         }
         
         Spacer()
