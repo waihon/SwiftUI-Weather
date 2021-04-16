028ae21b02ea137522f4aa5f26ec7f9b88ab3a3f
Waihon Yew <yewwaihon@gmail.com>
2021-04-16 21:00:42 +0800

    Extract WeatherButton to a separate SwiftUI view file for reusability

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -127,19 +127,3 @@ struct MainWeatherStatusView: View {
     .padding(.bottom, 40)
   }
 }
-
-struct WeatherButton: View {
-  
-  var title: String
-  var textColor: Color
-  var backgroundColor: Color
-  
-  var body: some View {
-    Text(title)
-      .frame(width: 280, height: 50)
-      .background(backgroundColor)
-      .foregroundColor(textColor)
-      .font(.system(size: 20, weight: .bold, design: .default))
-      .cornerRadius(10)
-  }
-}
--- SwiftUI-Weather/WeatherButton.swift
+++ SwiftUI-Weather/WeatherButton.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct WeatherButton: View {
+  
+  var title: String
+  var textColor: Color
+  var backgroundColor: Color
+  
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    Text(title)
+      .frame(width: 280, height: 50)
+      .background(backgroundColor)
+      .foregroundColor(textColor)
+      .font(.system(size: 20, weight: .bold, design: .default))
+      .cornerRadius(10)
   }
 }
 
 struct WeatherButton_Previews: PreviewProvider {
   static var previews: some View {
-    WeatherButton()
-  }
+    WeatherButton(title: "Change Day Time",
+                  textColor: .blue,
+                  backgroundColor: .white)  }
 }
