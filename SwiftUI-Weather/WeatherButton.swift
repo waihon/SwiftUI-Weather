f69964a4c3a774c015f1e2bb640311ff0eb63d1f
Waihon Yew <yewwaihon@gmail.com>
2021-04-16 20:58:06 +0800

    Create a new SwiftUI View

---
+++ SwiftUI-Weather/WeatherButton.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherButton.swift
+//  SwiftUI-Weather
+//
+//  Created by Yew Wai Hon on 16/04/2021.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct WeatherButton_Previews: PreviewProvider {
+  static var previews: some View {
+    WeatherButton()
+  }
+}
